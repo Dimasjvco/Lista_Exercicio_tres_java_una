edc9def6355e71179adc01fa4c8716b43ae1b7cb
João Victor Carvalho Dimas de Oliveira <130412515+Dimasjvco@users.noreply.github.com>
2023-04-29 17:24:36 -0300

    Update Exercicio4.java

--- Exercicio4.java
+++ Exercicio4.java
@@ -1,8 +1,7 @@
-//* Faça um programa que leia 5 números e informe a soma e a média dos números. *//
 
 import java.util.Scanner;
 
-public class ExercicioQuatro {
+public class Exercicio4 {
     public static void main(String[] args) {
 
     double numeroUm, numeroDois, numeroTres, numeroQuatro, numeroCinco, soma, media;
